bf42cd3f6c1741ad3a5d6f748dd8e083956151cf
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-07-06 15:03:03 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,12 @@
+#Write a program to do addition/division
+
+n1 = int(input("Enter the 1st no.: "))
+n2 = int(input("Enter the 2nd no.: "))
+s = n1+n2
+print(f"sum: {s}")
+
+if n2==0:
+    print("Error: Division not possible with 0")
+else:
+    d=n1/n2
+    print(f"division: {d}")
---
+++ Q3.py
@@ -0,0 +1,6 @@
+#Write a program to find the area of triangle
+
+b = float(input("Enter the base: "))
+h = float(input("Enter the height: "))
+a = 0.5*b*h
+print(f"area of triangle: {a}")
